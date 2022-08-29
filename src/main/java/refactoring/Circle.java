60ceea71086f1c2eb08e82bdb6c72c4f135a61e2
Oliver Zihler <oliver.zihler1@swisscom.com>
2022-08-29 07:47:50 +0200

    after renaming.

---
+++ src/main/java/refactoring/Circle.java
@@ -0,0 +1,39 @@
+package refactoring;
+
+public class Circle {
+
+    private int centerX;
+    private int centerY;
+    private int radius;
+
+    public Circle(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public int countContainingPoints(int[] xCords, int[] yCords) {
+        int numberOfPoints = 0;
+        for (int i = 0; i < xCords.length; ++i) {
+            if ((xCords[i] - this.centerX) * (xCords[i] - this.centerX) + (yCords[i] - this.centerY) * (yCords[i] - this.centerY) <= radius * radius) {
+                numberOfPoints++;
+            }
+        }
+        return numberOfPoints;
+    }
+
+
+    public void moveTo(int x, int y) {
+        this.centerX = x;
+        this.centerY = y;
+    }
+
+    public void resize(int r) {
+        this.radius = r;
+    }
+
+    public String format() {
+        return "(" + centerX + ", " + centerY + "), " + radius;
+    }
+
+}
---
+++ src/test/java/refactoring/CircleTest.java
@@ -0,0 +1,47 @@
+package refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+    @Test
+    void should_count_containing_points() {
+        Circle circle = new Circle(0, 0, 20);
+        int[] xCoordinates = new int[]{2, 3, 4, -12, -20};
+        int[] yCoordinates = new int[]{8, 20, 15, -4, -20};
+        assertEquals(3, circle.countContainingPoints(xCoordinates, yCoordinates));
+    }
+
+    @Test
+    void should_move() {
+        Circle Circle = new Circle(0, 0, 20);
+        int[] xCoordinates = new int[]{2, 3, 4, -12, -20};
+        int[] yCoordinates = new int[]{8, 20, 15, -4, -20};
+        assertEquals(3, Circle.countContainingPoints(xCoordinates, yCoordinates));
+
+        Circle.moveTo(-30, -30);
+
+        assertEquals(1, Circle.countContainingPoints(xCoordinates, yCoordinates));
+    }
+
+    @Test
+    void should_resize() {
+        Circle Circle = new Circle(0, 0, 20);
+        int[] xCoordinates = new int[]{2, 3, 4, -12, -20};
+        int[] yCoordinates = new int[]{8, 20, 15, -4, -20};
+        assertEquals(3, Circle.countContainingPoints(xCoordinates, yCoordinates));
+
+        Circle.resize(40);
+        assertEquals(5, Circle.countContainingPoints(xCoordinates, yCoordinates));
+
+        Circle.resize(1);
+        assertEquals(0, Circle.countContainingPoints(xCoordinates, yCoordinates));
+    }
+
+    @Test
+    void should_format() {
+        Circle circle = new Circle(1, 4, 7);
+        assertEquals("(1, 4), 7", circle.format());
+    }
+}
