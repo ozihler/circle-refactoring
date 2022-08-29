d4e5bb172bfe8e546920b7fbe4a2337f9682925f
Oliver Zihler <oliver.zihler1@swisscom.com>
2022-08-29 09:33:32 +0200

    rename variable.

--- src/test/java/refactoring/CircleTest.java
+++ src/test/java/refactoring/CircleTest.java
@@ -15,28 +15,28 @@ class CircleTest {
 
     @Test
     void should_move() {
-        Circle Circle = new Circle(new Point(0, 0), 20);
+        Circle circle = new Circle(new Point(0, 0), 20);
         int[] xCoordinates = new int[]{2, 3, 4, -12, -20};
         int[] yCoordinates = new int[]{8, 20, 15, -4, -20};
-        assertEquals(3, Circle.countContainingPoints(xCoordinates, yCoordinates));
+        assertEquals(3, circle.countContainingPoints(xCoordinates, yCoordinates));
 
-        Circle.moveTo(-30, -30);
+        circle.moveTo(-30, -30);
 
-        assertEquals(1, Circle.countContainingPoints(xCoordinates, yCoordinates));
+        assertEquals(1, circle.countContainingPoints(xCoordinates, yCoordinates));
     }
 
     @Test
     void should_resize() {
-        Circle Circle = new Circle(new Point(0, 0), 20);
+        Circle circle = new Circle(new Point(0, 0), 20);
         int[] xCoordinates = new int[]{2, 3, 4, -12, -20};
         int[] yCoordinates = new int[]{8, 20, 15, -4, -20};
-        assertEquals(3, Circle.countContainingPoints(xCoordinates, yCoordinates));
+        assertEquals(3, circle.countContainingPoints(xCoordinates, yCoordinates));
 
-        Circle.resize(40);
-        assertEquals(5, Circle.countContainingPoints(xCoordinates, yCoordinates));
+        circle.resize(40);
+        assertEquals(5, circle.countContainingPoints(xCoordinates, yCoordinates));
 
-        Circle.resize(1);
-        assertEquals(0, Circle.countContainingPoints(xCoordinates, yCoordinates));
+        circle.resize(1);
+        assertEquals(0, circle.countContainingPoints(xCoordinates, yCoordinates));
     }
 
     @Test
